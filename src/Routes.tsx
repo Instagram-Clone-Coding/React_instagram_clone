960e28a1d1a1e189c7c8017fe5693b781bf853d4
kimyoungyin <mafa1234@naver.com>
2021-09-24 19:34:04 +0900

    feat: 디렉토리 구조 변경 및 router 생성

---
+++ src/Routes.tsx
@@ -0,0 +1,14 @@
+import Home from "pages/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Home} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default Routes;
