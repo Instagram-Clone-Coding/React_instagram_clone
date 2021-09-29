63cd005ba5b70881d392b0a862834c555164bf6f
Suhwa <hansuhwa2005@gmail.com>
2021-09-29 21:38:17 +0900

    feat: router 추가

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,13 @@
 import Home from "pages/Home";
+import LoginPage from "pages/Login";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const Routes = () => {
     return (
         <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={LoginPage} />
+            </Switch>
             <Switch>
                 <Route exact path="/" component={Home} />
             </Switch>
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,5 @@
+function LoginPage() {
+    return <div>로그인페이지</div>;
+}
+
+export default LoginPage;
