c69f062a07aedf0314e6a0e47a3f6894e35209dc
Suhwa <hansuhwa2005@gmail.com>
2021-11-01 16:55:24 +0900

    feat: 이미지 sprite 처리함수 구현

--- src/components/Login/Forms.tsx
+++ src/components/Login/Forms.tsx
@@ -1,9 +1,21 @@
 import styled, { css } from "styled-components";
+import sprite from "images/Login/sprite.png";
+import { imageProps, ImgSprite } from "components/common/Sprite";
 
 export function Forms() {
     return (
-        <div>
-            <span>forms</span>
-        </div>
+        <ImgSprite
+            url={test.url}
+            width={test.width}
+            height={test.height}
+            position={test.position}
+        />
     );
 }
+
+const test: imageProps = {
+    url: sprite,
+    width: 175,
+    height: 51,
+    position: `0 -130px`,
+};
--- src/components/common/Sprite.tsx
+++ src/components/common/Sprite.tsx
@@ -1,5 +1,16 @@
-import sprite from "images/Login/sprite.png";
+import styled from "styled-components";
 
-export function RenderImage() {
-    return <div>image</div>;
+export interface imageProps {
+    url: string;
+    width: number;
+    height: number;
+    position: string;
 }
+
+export const ImgSprite = styled.div<imageProps>`
+    background-repeat: no-repeat;
+    background: url(${(props) => props.url});
+    width: ${(props) => props.width}px;
+    height: ${(props) => props.height}px;
+    background-position: ${(props) => props.position};
+`;
