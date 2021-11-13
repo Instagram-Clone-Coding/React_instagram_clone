d324397aa1049064fd9ce562938864105d3a291b
suhwa <hansuhwa2005@gmail.com>
2021-11-13 21:50:00 +0900

    fix: 사용안한 css 삭제

--- src/components/Login/Footer.tsx
+++ src/components/Login/Footer.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export function Footer() {
     return <div>footer</div>;
--- src/components/Login/LoginForm/LoginForm.tsx
+++ src/components/Login/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { Forms } from "./Forms";
 import { Suggest } from "../../common/Suggest";
 import apple from "images/Login/appStore.png";
