46ebc360e88742f06b70d4148a5ff3b6d8565340
suhwa <hansuhwa2005@gmail.com>
2021-11-21 16:18:14 +0900

    etc: 이미지 sprite 이름 변경

--- src/components/common/Sprite.tsx
+++ src/components/common/Sprite.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import sprite from "assets/images/sprite.png";
+import sprite from "assets/images/LoginSprite.png";
 import { imageProps } from "components/Login/types";
 
 const ImgSprite = styled.div<imageProps>`
