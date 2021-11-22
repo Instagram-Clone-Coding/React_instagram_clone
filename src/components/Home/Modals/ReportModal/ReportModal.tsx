4230c272673029d3a81509e8ab00bf1aa6eb425c
kimyoungyin <yin199859@gmail.com>
2021-11-22 23:02:13 +0900

    etc: prettier svg

--- src/components/Home/Modals/ReportModal/ReportModal.tsx
+++ src/components/Home/Modals/ReportModal/ReportModal.tsx
@@ -18,7 +18,33 @@ const REPORT_REASONS = [
     "마음에 들지 않습니다",
 ];
 
-const ReportModalInner = styled.div``;
+const ReportModalInner = styled.div`
+    & > * {
+        border-bottom: ${(props) => props.theme.color.bd_gray} 1px solid;
+    }
+    & > h4 {
+        margin-top: 24px;
+        padding: 0 16px;
+        padding-bottom: 16px;
+        font-size: 16px;
+        line-height: 24px;
+        font-weight: ${(props) => props.theme.font.bold};
+    }
+    & > button {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        padding: 16px;
+        font-weight: normal;
+        & > span {
+            flex: 1 1 auto;
+            text-align: start;
+        }
+        & > div {
+            transform: rotate(90deg);
+        }
+    }
+`;
 
 interface ReportModalProps {
     onModalOn: () => void;
@@ -34,7 +60,7 @@ const ReportModal = ({ onModalOn, onModalOff }: ReportModalProps) => {
         >
             <ReportModalInner>
                 <ModalHeader title="신고" onModalOff={onModalOff} />
-                <h2>이 게시물을 신고하는 이유</h2>
+                <h4>이 게시물을 신고하는 이유</h4>
                 {REPORT_REASONS.map((reason, index) => (
                     <button key={index}>
                         <span>{reason}</span>
@@ -43,6 +69,147 @@ const ReportModal = ({ onModalOn, onModalOff }: ReportModalProps) => {
                         </div>
                     </button>
                 ))}
+                <div className="loading">
+                    <svg
+                        aria-label="읽어들이는 중..."
+                        viewBox="0 0 100 100"
+                        // width="17"
+                        // height="17"
+                    >
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(-90 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0.08333333333333333"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(-60 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0.16666666666666666"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(-30 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0.25"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(0 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0.3333333333333333"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(30 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0.4166666666666667"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(60 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0.5"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(90 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0.5833333333333334"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(120 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0.6666666666666666"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(150 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0.75"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(180 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0.8333333333333334"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(210 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                        <rect
+                            fill="#555555"
+                            height="6"
+                            opacity="0.9166666666666666"
+                            rx="3"
+                            ry="3"
+                            transform="rotate(240 50 50)"
+                            width="25"
+                            x="72"
+                            y="47"
+                        ></rect>
+                    </svg>
+                </div>
             </ReportModalInner>
         </ModalCard>
     );
