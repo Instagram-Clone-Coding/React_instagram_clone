8c1083edd8dbbb4295ecf4faadf2a78c0cbb9490
kimyoungyin <yin199859@gmail.com>
2021-12-28 14:54:42 +0900

    fix: articleModal branch 머지 전 절대경로로 변경 및 index.js 작업

--- src/components/Home/Article/Article.tsx
+++ src/components/Home/Article/Article.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Card from "UI/Card/Card";
+import Card from "styles/UI/Card";
 import { useEffect, useState } from "react";
 import ArticleHeader from "./ArticleHeader";
 import ArticleImgSlider from "./ArticleImgSlider";
