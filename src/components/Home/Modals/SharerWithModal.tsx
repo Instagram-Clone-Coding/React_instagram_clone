fa7a59b97b1ff4535a4728adfb1768c298661bb7
kimyoungyin <yin199859@gmail.com>
2022-01-21 21:53:16 +0900

    refactor: 디렉토리 컨벤션에 따라 Hooks 폴더를 hooks로 변경

--- src/components/Home/Modals/SharerWithModal.tsx
+++ src/components/Home/Modals/SharerWithModal.tsx
@@ -3,7 +3,7 @@ import ModalCard from "styles/UI/ModalCard";
 import ModalHeader from "./ModalHeader";
 import direct from "../../../assets/Images/direct.png";
 import sprite3 from "../../../assets/Images/sprite3.png";
-import useCopy from "Hooks/useCopy";
+import useCopy from "hooks/useCopy";
 
 const ShareWithModalInner = styled.div`
     .shareWithModal-options {
