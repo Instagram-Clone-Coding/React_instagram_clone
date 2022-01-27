fa3b29b3a3a79978d59854a6e45f17091ead5b91
park chanhyuk <pgg6713@gmail.com>
2022-01-27 09:17:56 +0900

    fix: useGapText 홈페이지와 DM 호환문제 해결

--- src/components/Home/Article/Article.tsx
+++ src/components/Home/Article/Article.tsx
@@ -138,7 +138,7 @@ const Article = ({ article, isObserving, isLast }: ArticleComponentPros) => {
                 commentsCount={article.postCommentsCount}
                 // comments={article.comments}
             />
-            <div className="article-createdAt">{gapText}</div>
+            <div className="article-createdAt">{gapText}전</div>
             <div className="article-form-layout">
                 <CommentForm />
             </div>
