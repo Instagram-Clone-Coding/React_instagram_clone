2d4add421e416a47f23e15c03ba622e09b644c7e
kimyoungyin <yin199859@gmail.com>
2022-02-05 21:51:52 +0900

    feat: 토큰 관련 에러 메세지 상수 선언
    utils/constant.ts

---
+++ src/utils/constant.ts
@@ -0,0 +1,4 @@
+export const INVALID_TOKEN_MESSAGE = "유효하지 않은 토큰입니다.";
+export const EXPIRED_TOKEN_MESSAGE =
+    "만료된 REFRESH 토큰입니다. 재로그인 해주십시오.";
+export const FAIL_TO_REISSUE_MESSAGE = "fail to reIssue";
