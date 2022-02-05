2279e71b85f886e272dc25583eebd5900d4e096f
kimyoungyin <yin199859@gmail.com>
2022-02-06 00:49:42 +0900

    feat: token이 필요한 요청에 대해 authorizedCustomAxios.interceptors로 request를 가져와 토큰 재발급 처리

--- src/customAxios.ts
+++ src/customAxios.ts
@@ -1,5 +1,10 @@
 import { Token } from "app/store/ducks/auth/authThunk.type";
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import {
+    EXPIRED_TOKEN_MESSAGE,
+    FAIL_TO_REISSUE_MESSAGE,
+    INVALID_TOKEN_MESSAGE,
+} from "utils/constant";
 
 var jwt = require(`jsonwebtoken`);
 
@@ -25,19 +30,26 @@ export const checkToken = async (config: AxiosRequestConfig) => {
     // 토큰 만료시간이 지났다면 !authorizedCustomAxios.defaults.headers
     if (decode.exp < nowDate) {
         try {
-            const {
-                data: { type, accessToken },
-            }: AuthType.Token = await customAxios.post(`/reissue`);
-
-            authorizedCustomAxios.defaults.headers.common[
-                `Authorization`
-            ] = `${type} ${accessToken}`;
-
-            if (config.headers) {
-                config.headers[`Authorization`] = `${type} ${accessToken}`;
+            const { data, message }: AuthType.Token = await customAxios.post(
+                `/reissue`,
+            );
+            if (data) {
+                authorizedCustomAxios.defaults.headers.common[
+                    `Authorization`
+                ] = `${data.type} ${data.accessToken}`;
+                if (config.headers) {
+                    config.headers[
+                        `Authorization`
+                    ] = `${data.type} ${data.accessToken}`;
+                }
+            } else if (
+                message === INVALID_TOKEN_MESSAGE ||
+                message === EXPIRED_TOKEN_MESSAGE
+            ) {
+                return Promise.reject(FAIL_TO_REISSUE_MESSAGE);
             }
         } catch (error) {
-            return Promise.reject(error);
+            return Promise.reject(FAIL_TO_REISSUE_MESSAGE);
         }
     }
     // } else {
