75c5d610e065081ba7ede401eb7c4cc910c9ed19
kimyoungyin <yin199859@gmail.com>
2022-02-07 23:18:59 +0900

    fix: useInput onChangeValidator 추가된 것 반영

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,33 +1,51 @@
 import { ChangeEvent, useState } from "react";
 
+type ReturnType = [
+    Login.useInputProps,
+    boolean | null,
+    boolean,
+    () => void,
+    () => void,
+];
+
 const useInput = (
     initialValue: string,
     onBlurValidator?: (value: string) => boolean,
-) => {
+    onChangeValidator?: (value: string) => boolean,
+): ReturnType => {
     const [value, setValue] = useState(initialValue);
     const [isValid, setIsValid] = useState<boolean | null>(null);
+    const [isFocus, setIsFocus] = useState<boolean>(false);
 
     const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-        const {
-            target: { value },
-        } = event;
-        if (onBlurValidator) {
-            setIsValid(null);
-        }
-        setValue(value);
+        setValue(event.target.value);
+        onChangeValidator && setIsValid(onChangeValidator(value));
+    };
+
+    const onFocus = () => {
+        setIsFocus(true);
+        onBlurValidator && !isValid && setIsValid(null);
     };
 
     const resetValue = () => setValue("");
+    const resetIsValid = () => setIsValid(null);
 
     const onBlur = () => {
-        if (onBlurValidator) {
-            setIsValid(onBlurValidator(value));
-        }
+        setIsFocus(false);
+        onBlurValidator && setIsValid(onBlurValidator(value));
     };
 
-    return onBlurValidator
-        ? [resetValue, isValid, { value, onChange, onBlur }]
-        : [resetValue, { value, onChange }];
+    return [
+        onBlurValidator
+            ? { value, onChange, onBlur, onFocus }
+            : onChangeValidator
+            ? { value, onChange, onFocus }
+            : { value, onChange },
+        isValid,
+        isFocus,
+        resetValue,
+        resetIsValid,
+    ];
 };
 
 export default useInput;
