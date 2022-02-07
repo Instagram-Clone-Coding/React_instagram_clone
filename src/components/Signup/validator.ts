ff0e105c6b41f5e404cd563b2122a15d599bae07
suhwa <hansuhwa2005@gmail.com>
2022-02-07 19:35:21 +0900

    fix: validator 함수 이름 변경

--- src/components/Signup/validator.ts
+++ src/components/Signup/validator.ts
@@ -1,25 +1,25 @@
-const isValidLengthScope = (
+const lengthScopeValidator = (
     value: string,
     minLength: number,
     maxLength: number,
-): Boolean => {
+): boolean => {
     const Length = value.length;
     return Length >= minLength && Length <= maxLength;
 };
 
-export const isValidEmailForm = (email: string): Boolean => {
+export const emailFormValidator = (email: string): boolean => {
     const reg =
         /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
     const matchResult = email.match(reg);
     return matchResult === null ? false : true;
 };
 
-export const isValidName = (name: string): Boolean => {
-    return isValidLengthScope(name, 2, 8);
+export const nameValidator = (name: string): boolean => {
+    return lengthScopeValidator(name, 2, 8);
 };
 
-export const isValidUsername = (username: string): Boolean => {
-    if (!isValidLengthScope(username, 4, 12)) {
+export const usernameValidator = (username: string): boolean => {
+    if (!lengthScopeValidator(username, 4, 12)) {
         return false;
     }
 
@@ -30,8 +30,8 @@ export const isValidUsername = (username: string): Boolean => {
     return true;
 };
 
-export const isValidPassword = (password: string): Boolean => {
-    if (!isValidLengthScope(password, 4, 20)) {
+export const passwordValidator = (password: string): boolean => {
+    if (!lengthScopeValidator(password, 4, 20)) {
         return false;
     }
     return true;
