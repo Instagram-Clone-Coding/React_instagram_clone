1a5ee733d4d62ae3c7785a1e0b6479776c6a5418
kimyoungyin <yin199859@gmail.com>
2022-02-18 22:57:47 +0900

    feat: DragAndDrop 컴포넌트

---
+++ src/components/Common/Header/Upload/DragAndDrop/DragAndDrop.tsx
@@ -0,0 +1,193 @@
+import React, { ChangeEvent, DragEvent, useRef, useState } from "react";
+import { ReactComponent as ImgOrVideoIcon } from "assets/Svgs/imgOrVideo.svg";
+import Button from "styles/UI/Button";
+import { useAppDispatch, useAppSelector } from "app/store/Hooks";
+import { uploadActions } from "app/store/ducks/upload/uploadSlice";
+import styled from "styled-components";
+
+const StyledDragAndDrop = styled.div`
+    width: 100%;
+    height: 100%;
+    & > .upload__file {
+        padding: 24px;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        & > div {
+            margin-top: 16px;
+            & > h2 {
+                font-weight: 300;
+                font-size: 22px;
+                line-height: 26px;
+                text-align: center;
+            }
+        }
+        & > button {
+            margin-top: 24px;
+        }
+        &.dragOver {
+            background-color: ${(props) => props.theme.color.bg_gray};
+            & > svg {
+                color: ${(props) => props.theme.color.blue};
+            }
+        }
+    }
+    & > form > input {
+        display: none !important;
+    }
+`;
+
+const DragAndDrop = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    // const [files, setFiles] = useState<(string | ArrayBuffer | null)[]>([]);
+    const [isDraggingOver, setIsDraggingOver] = useState(false);
+    const dispatch = useAppDispatch();
+
+    const buttonClickHandler = () => {
+        if (inputRef.current) {
+            inputRef.current.click();
+        }
+    };
+
+    //drag한 마우스가 들어왔을 때
+    const dragInHandler = (event: DragEvent): void => {
+        event.preventDefault();
+        event.stopPropagation();
+    };
+
+    // drag한 마우스가 해당 요소 밖으로 벗어났을 때
+    const dragOutHandler = (event: DragEvent): void => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        setIsDraggingOver(false);
+    };
+
+    // drag한 마우스가 해당 요소 위에서 움직일 때(유지될 떄)
+    const dragOverHandler = (event: DragEvent): void => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (event.dataTransfer!.files) {
+            setIsDraggingOver(true);
+        }
+    };
+
+    const dropHandler = (event: DragEvent): void => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const droppedFiles = event.dataTransfer.files;
+        Array.from(droppedFiles).forEach((file) => {
+            const img = new Image();
+            img.src = URL.createObjectURL(file);
+            img.onload = () => {
+                // window.URL.revokeObjectURL(img.src);
+                dispatch(
+                    uploadActions.addFile({
+                        url: img.src,
+                        width: img.width,
+                        height: img.height,
+                    }),
+                );
+                dispatch(uploadActions.toCutStep());
+            };
+            // const reader = new FileReader();
+            // reader.readAsDataURL(file);
+            // reader.onloadend = () => {
+            //     const img = new Image();
+            //     if (reader.result) {
+            //         img.src = reader.result.toString();
+            //         img.onload = () => {
+            //             dispatch(
+            //                 uploadActions.addFile({
+            //                     url: img.src,
+            //                     width: img.width,
+            //                     height: img.height,
+            //                 }),
+            //             );
+            //             dispatch(uploadActions.toCutStep());
+            //         };
+            //     }
+            // };
+        });
+        setIsDraggingOver(false);
+    };
+
+    const fileInputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const addedFiles = event.target.files;
+        if (!addedFiles) return;
+        Array.from(addedFiles).forEach((file) => {
+            const img = new Image();
+            img.src = URL.createObjectURL(file);
+            img.onload = () => {
+                // window.URL.revokeObjectURL(img.src);
+                dispatch(
+                    uploadActions.addFile({
+                        url: img.src,
+                        width: img.width,
+                        height: img.height,
+                    }),
+                );
+                dispatch(uploadActions.toCutStep());
+            };
+            // const reader = new FileReader();
+            // reader.readAsDataURL(file);
+            // reader.onloadend = () => {
+            //     const img = new Image();
+            //     if (reader.result) {
+            //         img.src = reader.result.toString();
+            //         img.onload = () => {
+            //             dispatch(
+            //                 uploadActions.addFile({
+            //                     url: img.src,
+            //                     width: img.width,
+            //                     height: img.height,
+            //                 }),
+            //             );
+            //             dispatch(uploadActions.toCutStep());
+            //         };
+            //     }
+            // };
+        });
+    };
+
+    return (
+        <StyledDragAndDrop>
+            <div
+                className={`upload__file ${isDraggingOver ? "dragOver" : ""}`}
+                onDrop={dropHandler}
+                onDragEnter={dragInHandler}
+                onDragLeave={dragOutHandler}
+                onDragOver={dragOverHandler}
+            >
+                <ImgOrVideoIcon />
+                <div>
+                    <h2>사진과 동영상을 여기 끌어다 놓으세요</h2>
+                </div>
+                <Button type="button" onClick={buttonClickHandler}>
+                    컴퓨터에서 선택
+                </Button>
+            </div>
+            <form
+                encType="multipart/form-data"
+                method="POST"
+                role="presentation"
+            >
+                <input
+                    accept="image/jpeg,image/png,image/heic,image/heif"
+                    multiple={true}
+                    type="file"
+                    ref={inputRef}
+                    onChange={fileInputChangeHandler}
+                    // value={}
+                />
+            </form>
+        </StyledDragAndDrop>
+    );
+};
+
+export default DragAndDrop;
