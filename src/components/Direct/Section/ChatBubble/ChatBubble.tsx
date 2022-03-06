303a60040eae0eb0ae268d8624574fa2a433112a
Chanhyuk Park <pgg6713@gmail.com>
2022-03-06 11:45:43 +0900

    feat: 메시지 복사 구현 완료

--- src/components/Direct/Section/ChatBubble/ChatBubble.tsx
+++ src/components/Direct/Section/ChatBubble/ChatBubble.tsx
@@ -187,6 +187,45 @@ const ChatBubble = ({
     useEffect(() => {
         setShowGuide(selectedMessageId === messageId);
     }, [selectedMessageId]);
+
+
+    const copyhandler = () => {
+        // 흐음 1.
+        if (navigator.clipboard) {
+            // (IE는 사용 못하고, 크롬은 66버전 이상일때 사용 가능합니다.)
+            navigator.clipboard
+                .writeText(content as string)
+                .then(() => {
+                })
+                .catch(() => {
+                    alert("복사를 다시 시도해주세요.");
+                });
+        } else {
+            // 흐름 2.
+            if (!document.queryCommandSupported("copy")) {
+                return alert("복사하기가 지원되지 않는 브라우저입니다.");
+            }
+
+            // 흐름 3.
+            const textarea = document.createElement("textarea");
+            textarea.value = content as string;
+            textarea.style.top = "0";
+            textarea.style.left = "0";
+            textarea.style.position = "fixed";
+
+            // 흐름 4.
+            document.body.appendChild(textarea);
+            // focus() -> 사파리 브라우저 서포팅
+            textarea.focus();
+            // select() -> 사용자가 입력한 내용을 영역을 설정할 때 필요
+            textarea.select();
+            // 흐름 5.
+            document.execCommand("copy");
+            // 흐름 6.
+            document.body.removeChild(textarea);
+        }
+    };
+
     return (
         <ChatBubbleContainer me={me} ref={scrollRef}
                              onMouseEnter={(e: React.MouseEvent<HTMLDivElement>) => {
@@ -230,7 +269,8 @@ const ChatBubble = ({
                                         }
                                         }>좋아요</button>
                                 }
-                                <button>복사</button>
+                                {
+                                    typeof content === "string" && <button onClick={copyhandler}>복사</button>}
                                 <button onClick={() => {
                                     dispatch(openModal("deleteChatMessage"));
                                 }}>전송 취소
