c647912ed27f36a93089eac461110f8b12a28c4c
suhwa <hansuhwa2005@gmail.com>
2022-03-06 18:32:51 +0900

    fix: 로그인 에러처리 중 네트워크가 없을 경우 추가

--- src/app/store/ducks/auth/authSlice.ts
+++ src/app/store/ducks/auth/authSlice.ts
@@ -67,9 +67,11 @@ const authSlice = createSlice({
                 state.isLogin = true;
                 saveToken(action.payload);
             })
-            .addCase(signIn.rejected, (state) => {
+            .addCase(signIn.rejected, (state, action) => {
                 state.isAsyncReject = true;
-                if (state.hasUsername) {
+                if (action.payload && typeof action.payload === "string") {
+                    state.errorMessage = action.payload;
+                } else if (state.hasUsername) {
                     state.errorMessage = `잘못된 비밀번호입니다. 다시 확인하세요.`;
                 } else {
                     state.errorMessage = `입력한 사용자 이름을 사용하는 계정을 찾을 수 없습니다. 사용자 이름을 확인하고 다시 시도하세요.`;
