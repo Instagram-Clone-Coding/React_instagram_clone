450a72318ffe31eb2d97eecaf91ae5ae4e2b7c13
ChanhyukPark-Tech <pgg6713@gmail.com>
2022-03-11 11:57:04 +0900

    refactor: ChatBubble 프리티어 적용

--- src/components/Direct/Section/ChatBubble/ChatBubble.tsx
+++ src/components/Direct/Section/ChatBubble/ChatBubble.tsx
@@ -2,7 +2,10 @@ import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { useAppDispatch, useAppSelector } from "app/store/Hooks";
 import { ReactComponent as ThreeDots } from "assets/Svgs/threeDots.svg";
-import { openModal, setSelectedMessageId } from "app/store/ducks/direct/DirectSlice";
+import {
+    openModal,
+    setSelectedMessageId,
+} from "app/store/ducks/direct/DirectSlice";
 import moment from "moment";
 
 interface ChatBubbleProps {
@@ -20,161 +23,164 @@ interface ChatBubbleContainerType {
     me: boolean;
     showThreeDotsButton: boolean;
     showGuide: boolean;
-    onMouseEnter: (
-        event: React.MouseEvent<HTMLDivElement>,
-    ) => void;
-    onMouseLeave: (
-        event: React.MouseEvent<HTMLDivElement>,
-    ) => void;
+    onMouseEnter: (event: React.MouseEvent<HTMLDivElement>) => void;
+    onMouseLeave: (event: React.MouseEvent<HTMLDivElement>) => void;
     liked: AuthType.UserInfo | undefined;
 }
 
 const ChatBubbleContainer = styled.div<ChatBubbleContainerType>`
-  margin-top: 5px;
-  padding: 0px 20px;
-  padding-left: ${props => props.me ? "20px" : "50px"};
-  text-align: ${props => props.me ? "right" : "left"};
-  display: block;
-  position: relative;
-  margin-bottom: ${props => props.liked ? "15px" : "0"};
+    margin-top: 5px;
+    padding: 0px 20px;
+    padding-left: ${(props) => (props.me ? "20px" : "50px")};
+    text-align: ${(props) => (props.me ? "right" : "left")};
+    display: block;
+    position: relative;
+    margin-bottom: ${(props) => (props.liked ? "15px" : "0")};
 
-  .date-section {
-    width: 100%;
-    text-align: center;
-    font-size: 12px;
-    color: #8E8E8E;
-    margin: 10px 0;
-  }
+    .date-section {
+        width: 100%;
+        text-align: center;
+        font-size: 12px;
+        color: #8e8e8e;
+        margin: 10px 0;
+    }
 
-  .content {
+    .content {
+        display: flex;
+        justify-content: ${(props) => (props.me ? "end" : "start")};
+        flex-direction: ${(props) => !props.me && "row-reverse"};
+        align-items: center;
 
-    display: flex;
-    justify-content: ${props => props.me ? "end" : "start"};
-    flex-direction: ${props => !props.me && "row-reverse"};
-    align-items: center;
+        .guide-part {
+            position: relative;
 
-    .guide-part {
-      position: relative;
+            .guide-container {
+                position: absolute;
+                top: calc(50% - 58px);
+                z-index: ${(props) => (props.showGuide ? 2 : -1)};
+                right: ${(props) => props.me && "calc(100% - 34px)"};
+                left: ${(props) => !props.me && "calc(100% - 34px)"};
 
-      .guide-container {
+                .guide-inner {
+                    opacity: ${(props) => (props.showGuide ? 1 : 0)};
+                    transform: scale(${(props) => (props.showGuide ? 1 : 0)});
+                    transform-origin: bottom center;
+                    background-color: #000;
+                    color: #fff;
+                    border-radius: 8px;
+                    box-shadow: rgb(0 0 0 / 20%) 0 4px 22px;
+                    padding: 8px 12px;
+                    transition: opacity 0.3s
+                            cubic-bezier(0.175, 0.885, 0.32, 1.275),
+                        transform 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275),
+                        -webkit-transform 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+                    width: max-content;
+
+                    .arrow {
+                        position: absolute;
+                        width: 100%;
+                        bottom: -6px;
+                        left: ${(props) => props.me && "calc(50% - 20px)"};
+                        right: ${(props) => !props.me && "calc(50% - 20px)"};
+
+                        .arrow-detail {
+                            border-radius: 2px;
+                            height: 15px;
+                            margin: auto;
+                            transform: rotate(45deg);
+                            width: 15px;
+                            background-color: #000;
+                        }
+                    }
+
+                    .guide-content {
+                        button {
+                            color: white;
+                            margin: 0 4px;
+                        }
+                    }
+                }
+            }
+
+            svg {
+                display: ${(props) =>
+                    props.showThreeDotsButton || props.showGuide
+                        ? "block"
+                        : "none"};
+                margin: 0 5px;
+                fill: rgb(142, 142, 142);
+                cursor: pointer;
+
+                &:hover {
+                    fill: rgb(38, 38, 38);
+                }
+            }
+        }
+
+        p {
+            padding: 15px;
+            display: inline-block;
+            max-width: 234px;
+            text-align: left;
+            border-radius: 16px;
+            overflow-wrap: break-word;
+            white-space: normal;
+            background: ${(props) =>
+                props.me
+                    ? "rgba(var(--bb2, 239, 239, 239), 1)"
+                    : "transparent"};
+            border: ${(props) =>
+                props.me ? "none" : `1px solid rgba(0,0,0, 0.1)`};
+
+            img {
+                height: 200px;
+                min-height: 100%;
+                min-width: 100%;
+            }
+        }
+    }
+
+    & > img {
         position: absolute;
-        top: calc(50% - 58px);
-        z-index: ${props => props.showGuide ? 2 : -1};
-        right: ${props => props.me && 'calc(100% - 34px)'};
-        left: ${props => !props.me && 'calc(100% - 34px)'};
-
-
-        .guide-inner {
-          opacity: ${props => props.showGuide ? 1 : 0};
-          transform: scale(${props => props.showGuide ? 1 : 0});
-          transform-origin: bottom center;
-          background-color: #000;
-          color: #fff;
-          border-radius: 8px;
-          box-shadow: rgb(0 0 0 / 20%) 0 4px 22px;
-          padding: 8px 12px;
-          transition: opacity .3s cubic-bezier(.175, .885, .32, 1.275), transform .3s cubic-bezier(.175, .885, .32, 1.275), -webkit-transform .3s cubic-bezier(.175, .885, .32, 1.275);
-          width: max-content;
-
-          .arrow {
-            position: absolute;
-            width: 100%;
-            bottom: -6px;
-            left: ${props => props.me && 'calc(50% - 20px)'};
-            right: ${props => !props.me && 'calc(50% - 20px)'};
-
-            .arrow-detail {
-              border-radius: 2px;
-              height: 15px;
-              margin: auto;
-              transform: rotate(45deg);
-              width: 15px;
-              background-color: #000;
-            }
-          }
-
-          .guide-content {
-            button {
-              color: white;
-              margin: 0 4px;
-            }
-          }
-        }
-      }
-
-      svg {
-        display: ${props => props.showThreeDotsButton || props.showGuide ? "block" : "none"};
-        margin: 0 5px;
-        fill: rgb(142, 142, 142);
-        cursor: pointer;
-
-        &:hover {
-          fill: rgb(38, 38, 38);
-        }
-      }
-
+        bottom: 0;
+        left: 20px;
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+        object-fit: cover;
     }
 
-
-    p {
-      padding: 15px;
-      display: inline-block;
-      max-width: 234px;
-      text-align: left;
-      border-radius: 16px;
-      overflow-wrap: break-word;
-      white-space: normal;
-      background: ${props => props.me ? "rgba(var(--bb2, 239, 239, 239), 1)" : "transparent"};
-      border: ${props => props.me ? "none" : `1px solid rgba(0,0,0, 0.1)`};
-
-      img {
-        height: 200px;
-        min-height: 100%;
-        min-width: 100%;
-      }
+    .heart {
+        background-color: #efefef;
+        border-radius: 50%;
+        border: 2px solid #fff;
+        position: absolute;
+        right: ${(props) => props.me && 0};
+        left: ${(props) => !props.me && "50px"};
+        padding: 6px;
+        bottom: -14px;
     }
-  }
-
-  & > img {
-    position: absolute;
-    bottom: 0;
-    left: 20px;
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-    object-fit: cover;
-  }
-
-  .heart {
-    background-color: #efefef;
-    border-radius: 50%;
-    border: 2px solid #fff;
-    position: absolute;
-    right: ${props => props.me && 0};
-    left : ${props => !props.me && '50px'};
-    padding: 6px;
-    bottom: -14px;
-  }
-
 `;
 
-
 const ChatBubble = ({
-                        me,
-                        content,
-                        showDate,
-                        messageDate,
-                        messageId,
-                        likeMessageHandler,
-                        unlikeMessageHandler,
-                        likeMembers,
-                    }: ChatBubbleProps) => {
-    const [showThreeDotsButton, setShowThreeDotsButton] = useState<boolean>(false);
+    me,
+    content,
+    showDate,
+    messageDate,
+    messageId,
+    likeMessageHandler,
+    unlikeMessageHandler,
+    likeMembers,
+}: ChatBubbleProps) => {
+    const [showThreeDotsButton, setShowThreeDotsButton] =
+        useState<boolean>(false);
     const [showGuide, setShowGuide] = useState<boolean>(false);
     const scrollRef = useRef() as React.MutableRefObject<HTMLDivElement>;
-    const renewScroll = useAppSelector(state => state.direct.renewScroll);
-    const selectedMessageId = useAppSelector(state => state.direct.selectedMessageId);
-    const userInfo = useAppSelector(state => state.auth.userInfo);
+    const renewScroll = useAppSelector((state) => state.direct.renewScroll);
+    const selectedMessageId = useAppSelector(
+        (state) => state.direct.selectedMessageId,
+    );
+    const userInfo = useAppSelector((state) => state.auth.userInfo);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
@@ -184,7 +190,7 @@ const ChatBubble = ({
     }, [content]);
 
     // 내가 좋아요 했는지 판단하는 상수
-    const liked = likeMembers.find(member => {
+    const liked = likeMembers.find((member) => {
         return member.memberId === userInfo?.memberId;
     });
 
@@ -192,15 +198,13 @@ const ChatBubble = ({
         setShowGuide(selectedMessageId === messageId);
     }, [selectedMessageId]);
 
-
     const copyhandler = () => {
         // 흐음 1.
         if (navigator.clipboard) {
             // (IE는 사용 못하고, 크롬은 66버전 이상일때 사용 가능합니다.)
             navigator.clipboard
                 .writeText(content as string)
-                .then(() => {
-                })
+                .then(() => {})
                 .catch(() => {
                     alert("복사를 다시 시도해주세요.");
                 });
@@ -231,26 +235,34 @@ const ChatBubble = ({
     };
 
     return (
-        <ChatBubbleContainer me={me} ref={scrollRef}
-                             onMouseEnter={(e: React.MouseEvent<HTMLDivElement>) => {
-                                 e.preventDefault();
-                                 setShowThreeDotsButton(true);
-                             }}
-                             onMouseLeave={(e: React.MouseEvent<HTMLDivElement>) => {
-                                 e.preventDefault();
-                                 setShowThreeDotsButton(false);
-                             }}
-                             showThreeDotsButton={showThreeDotsButton}
-                             showGuide={showGuide}
-                             liked={liked}
+        <ChatBubbleContainer
+            me={me}
+            ref={scrollRef}
+            onMouseEnter={(e: React.MouseEvent<HTMLDivElement>) => {
+                e.preventDefault();
+                setShowThreeDotsButton(true);
+            }}
+            onMouseLeave={(e: React.MouseEvent<HTMLDivElement>) => {
+                e.preventDefault();
+                setShowThreeDotsButton(false);
+            }}
+            showThreeDotsButton={showThreeDotsButton}
+            showGuide={showGuide}
+            liked={liked}
         >
-            {
-                showDate && <div className={"date-section"}>{moment(messageDate).format("YYYY년 M월 DD일 a  h:mm")}</div>
-            }
-            {
-                !me &&
-                <img src={"https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=150"} alt={"보낸사람"} />
-            }
+            {showDate && (
+                <div className={"date-section"}>
+                    {moment(messageDate).format("YYYY년 M월 DD일 a  h:mm")}
+                </div>
+            )}
+            {!me && (
+                <img
+                    src={
+                        "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=150"
+                    }
+                    alt={"보낸사람"}
+                />
+            )}
 
             <div className={"content"}>
                 <div className={"guide-part"}>
@@ -260,52 +272,73 @@ const ChatBubble = ({
                                 <div className="arrow-detail"></div>
                             </div>
                             <div className="guide-content">
-
-                                {
-                                    liked ? <button onClick={() => {
+                                {liked ? (
+                                    <button
+                                        onClick={() => {
                                             unlikeMessageHandler();
                                             setShowGuide(false);
-                                        }
-                                        }>좋아요 취소</button>
-                                        : <button onClick={() => {
+                                        }}
+                                    >
+                                        좋아요 취소
+                                    </button>
+                                ) : (
+                                    <button
+                                        onClick={() => {
                                             likeMessageHandler();
                                             setShowGuide(false);
-                                        }
-                                        }>좋아요</button>
-                                }
-                                {
-                                    typeof content === "string" && <button onClick={copyhandler}>복사</button>}
-                                {me ? <button onClick={() => {
-                                    dispatch(openModal("deleteChatMessage"));
-                                }}>전송 취소
-                                </button> : <button onClick={() => {
-                                    console.log("신고로직구현")
-                                }}>신고
-                                </button>}
+                                        }}
+                                    >
+                                        좋아요
+                                    </button>
+                                )}
+                                {typeof content === "string" && (
+                                    <button onClick={copyhandler}>복사</button>
+                                )}
+                                {me ? (
+                                    <button
+                                        onClick={() => {
+                                            dispatch(
+                                                openModal("deleteChatMessage"),
+                                            );
+                                        }}
+                                    >
+                                        전송 취소
+                                    </button>
+                                ) : (
+                                    <button
+                                        onClick={() => {
+                                            console.log("신고로직구현");
+                                        }}
+                                    >
+                                        신고
+                                    </button>
+                                )}
                             </div>
                         </div>
                     </div>
-                    <ThreeDots onClick={() => dispatch(setSelectedMessageId(messageId))} />
+                    <ThreeDots
+                        onClick={() =>
+                            dispatch(setSelectedMessageId(messageId))
+                        }
+                    />
                 </div>
                 <p>
-                    {
-                        typeof content === "string" ? <> {content}</> :
-                            <img src={content.postImage.imageUrl} alt={content.postImage.imageName} />
-                    }
+                    {typeof content === "string" ? (
+                        <> {content}</>
+                    ) : (
+                        <img
+                            src={content.postImage.imageUrl}
+                            alt={content.postImage.imageName}
+                        />
+                    )}
                 </p>
-
             </div>
             {/*그 메세지에 좋아요를 누른 사람중에 내가 있다면 하트를 표시해주자*/}
-            {
-                liked &&
+            {liked && (
                 <div className={"heart"}>
-                    <div>
-                        ❤️
-                    </div>
+                    <div>❤️</div>
                 </div>
-            }
-
-
+            )}
         </ChatBubbleContainer>
     );
 };
