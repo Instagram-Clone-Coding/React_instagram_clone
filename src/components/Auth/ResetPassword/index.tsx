92428d09aea4a1a36a90b019d2609f6a5fd52eaa
suhwa <hansuhwa2005@gmail.com>
2022-03-16 14:25:08 +0900

    feat: 비밀번호 재설정 ui 구현

--- src/components/Auth/ResetPassword/index.tsx
+++ src/components/Auth/ResetPassword/index.tsx
@@ -3,6 +3,13 @@ import styled from "styled-components";
 import navLogo from "assets/Images/nav-logo.png";
 import { Link } from "react-router-dom";
 import ContentBox from "components/Common/ContentBox";
+import ImageSprite from "components/Common/ImageSprite";
+import sprite from "assets/Images/sprite2.png";
+import Input from "components/Common/Input";
+import useInput from "hooks/useInput";
+import SubmitButton from "../SubmitButton";
+import { MouseEvent } from "react";
+import Line from "../Line";
 
 // flow
 // 이메일 전송 [이메일폼 화면, 이메일화면]
@@ -25,32 +32,101 @@ const Container = styled.section`
         width: 100%;
         z-index: 101;
 
-        .image-container {
+        .logo {
             display: flex;
         }
     }
 
+    .fake-header {
+        height: 60px;
+    }
+
     .form-container {
-        position: relative;
-        margin: 44px 0;
-        .form-style {
-            margin: 60px auto 0;
+        margin: 60px auto 44px;
+        max-width: 388px;
+
+        .form-content {
+            display: flex;
+            flex-direction: column;
+            .image-container {
+                display: flex;
+                justify-content: center;
+                margin: 24px 0 16px 0;
+                .image {
+                    background-size: 440px 411px;
+                }
+            }
+
+            .description {
+                text-align: center;
+
+                h4 {
+                    font-weight: 600;
+                    margin: -6px 44px 10px 44px;
+                    font-size: 16px;
+                    line-height: 24px;
+                }
+
+                & > div {
+                    color: #8e8e8e;
+                    font-weight: 400;
+                    margin: -3px 44px 12px 44px;
+                    font-size: 14px;
+                    line-height: 18px;
+                }
+            }
+
+            a {
+                font-size: 14px;
+                font-weight: 600;
+                text-align: center;
+                text-decoration: none;
+            }
+
+            .comeback-signup {
+                margin: 0 44px 16px 44px;
+                text-align: center;
+            }
+
+            .comeback-login {
+                margin-top: 68px;
+                .login-box {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    height: 44px;
+                    background-color: #fafafa;
+                    border: 1px solid #dbdbdb;
+                }
+            }
         }
     }
 `;
 
+const image: Common.ImageProps = {
+    width: 96,
+    height: 96,
+    position: `-129px 0`,
+    url: sprite,
+};
+
 export default function ResetPasswordForm() {
-    // form
-    // - 이미지
-    // - 폼 & 버튼
-    // 또는
-    // 새계정만들기(회원가입)
-    // 로그인으로 돌아가기(로그인)
+    const [inputProps, isValid, isFocus] = useInput(
+        "",
+        undefined,
+        (value) => value.length > 0,
+    );
+
+    const submitButtonClickHandler = (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        // api 연결
+    };
+
     return (
         <Container>
-            <nav className="header">
+            <header className="header">
                 <Link to="/">
-                    <div className="image-contaienr">
+                    <div className="logo">
                         <img
                             src={navLogo}
                             alt="상단바 인스타 로고"
@@ -58,13 +134,51 @@ export default function ResetPasswordForm() {
                         />
                     </div>
                 </Link>
-            </nav>
+            </header>
+            <div className="fake-header" />
             <main className="form-container">
-                <div className="form-style">
-                    <ContentBox padding="0" margin="0 0 0 0">
-                        <div>reset Password form</div>
-                    </ContentBox>
-                </div>
+                <ContentBox padding="0" margin="0 0 0 0">
+                    <div className="form-content">
+                        <div className="image-container">
+                            <ImageSprite {...image} className="image" />
+                        </div>
+                        <div className="description">
+                            <h4>로그인에 문제가 있나요?</h4>
+                            <div>
+                                이메일 주소, 전화번호 또는 사용자 이름을
+                                입력하시면 계정에 다시 액세스할 수 있는 링크를
+                                보내드립니다.
+                            </div>
+                        </div>
+                        <Input
+                            type="text"
+                            inputName="username"
+                            innerText="사용자 이름"
+                            isFocus={isFocus}
+                            inputProps={inputProps}
+                        />
+                        <SubmitButton
+                            type="submit"
+                            onClick={submitButtonClickHandler}
+                            disabled={!isValid}
+                        >
+                            로그인 링크 보내기
+                        </SubmitButton>
+                        <Line margin="16px 44px" />
+                        <div className="comeback-signup">
+                            <Link to="/accounts/emailsignup/">
+                                새 계정 만들기
+                            </Link>
+                        </div>
+                        <div className="comeback-login">
+                            <div className="login-box">
+                                <Link to="/accounts/login">
+                                    로그인으로 돌아가기
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </ContentBox>
             </main>
             <Footer />
         </Container>
