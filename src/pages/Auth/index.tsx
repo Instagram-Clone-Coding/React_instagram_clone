920109f13ad66f2e3094cc2bd1b94bef33b0f3ae
suhwa <hansuhwa2005@gmail.com>
2022-07-07 14:55:47 +0900

    fix: 코드를 통한 로그인 엔드포인트 변경 적용
    로그인 후, redirect 없앴기에 코드를 통한 로그인 뒤에 url 루트로 변경하도록 수정함

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -3,10 +3,11 @@ import { useAppDispatch } from "app/store/Hooks";
 import Form from "components/Auth/Form";
 import { Footer } from "components/Common/Footer/Footer";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import queryString from "query-string";
 import { signInUseCode } from "app/store/ducks/auth/authThunk";
+import InstagramLoading from "InstagramLoading";
 
 const Section = styled.section`
     flex-shrink: 0;
@@ -22,24 +23,42 @@ const Section = styled.section`
 
 export default function AuthPage(props: { router: "signIn" | "signUp" }) {
     const dispatch = useAppDispatch();
+    const history = useHistory();
     const { search } = useLocation();
     const { username, code } = queryString.parse(
         search,
     ) as AuthType.resetPasswordQuery;
 
     useEffect(() => {
-        if (props.router === "signIn") {
-            dispatch(signInUseCode({ username, code }));
+        const checkLoginUseCode = async () => {
+            try {
+                await dispatch(signInUseCode({ username, code })).unwrap();
+                history.push("/");
+            } catch (error) {
+                // 에러인 경우(만료된 code) => 404페이지(만료된 페이지입니다)
+                history.push("/error");
+            }
+        };
+
+        if (username && code) {
+            checkLoginUseCode();
+        } else {
+            dispatch(authAction.changeFormState(props.router));
         }
-        dispatch(authAction.changeFormState(props.router));
     }, []);
 
     return (
-        <Section>
-            <main className="form-container">
-                <Form router={props.router} />
-            </main>
-            <Footer />
-        </Section>
+        <>
+            {username && code ? (
+                <InstagramLoading />
+            ) : (
+                <Section>
+                    <main className="form-container">
+                        <Form router={props.router} />
+                    </main>
+                    <Footer />
+                </Section>
+            )}
+        </>
     );
 }
