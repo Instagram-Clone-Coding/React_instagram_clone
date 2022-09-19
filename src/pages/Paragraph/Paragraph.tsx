5e36a2f613d24d362c27e7e32a157cc89d60613f
kimyoungyin <yin199859@gmail.com>
2022-09-19 17:37:54 +0900

    feat: status가 200이 아니라면 뒤로 가도록 history.goBack 사용

--- src/pages/Paragraph/Paragraph.tsx
+++ src/pages/Paragraph/Paragraph.tsx
@@ -3,7 +3,7 @@ import { useAppDispatch } from "app/store/Hooks";
 import OnlyArticlePage from "components/Common/Article/OnlyArticlePage";
 import { authorizedCustomAxios } from "customAxios";
 import React, { useEffect, useMemo, useState } from "react";
-import { useParams } from "react-router";
+import { useParams, useHistory } from "react-router";
 interface ArticleProps {
     data: PostType.ArticleProps;
 }
@@ -12,7 +12,9 @@ const Paragraph = () => {
     const [isFetching, setIsFetching] = useState(true);
     const dispatch = useAppDispatch();
     const { postId: postIdStr } = useParams<{ postId: string }>();
+    const history = useHistory();
     const postId = useMemo(() => +postIdStr, [postIdStr]);
+
     useEffect(() => {
         const getArticle = async () => {
             try {
@@ -22,7 +24,7 @@ const Paragraph = () => {
                 } = await authorizedCustomAxios.get<ArticleProps>(
                     `/posts/${postId}`,
                 );
-                // if (status !== 200) throw "Hello";
+                if (status === 200) history.goBack(); // 에러 발생 시 뒤로 이동
                 dispatch(postActions.getArticle(data));
             } catch (error) {
                 console.log(error);
@@ -31,7 +33,7 @@ const Paragraph = () => {
             }
         };
         getArticle();
-    }, [postId, dispatch]);
+    }, [postId, dispatch, history]);
     return <div>{isFetching || <OnlyArticlePage />}</div>;
 };
 
