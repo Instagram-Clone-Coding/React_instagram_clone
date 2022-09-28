acb0b994ee73c5ccd692f35d192c86776f094fb0
kimyoungyin <yin199859@gmail.com>
2022-09-29 01:27:41 +0900

    fix: 잘못된 home 게시물 타입 수정

--- src/@type/index.d.ts
+++ src/@type/index.d.ts
@@ -138,7 +138,7 @@ declare module HomeType {
     type StoriesScrollPositionType = "left" | "right" | "center";
     interface homeStateProps {
         storiesScrollPosition: storiesScrollPositionType;
-        articles: ArticleStateProps[];
+        articles: PostType.ArticleStateProps[];
         // location?
         isLoading: boolean; // 더미 로딩
         isExtraArticleLoading: boolean;
@@ -188,11 +188,11 @@ declare module PostType {
         mentionsOfContent: string[];
         likeOptionFlag: boolean; // 업로드한 사람만 좋아요 및 좋아요한 사람 확인 가능
         commentOptionFlag: boolean; // 댓글 작성 가능 여부
+        following: boolean;
         // recentComments: string[]; 내부 객체 구조
     }
 
     interface ArticleStateProps extends ArticleProps {
-        isFollowing: boolean;
         followLoading: boolean;
     }
 }
