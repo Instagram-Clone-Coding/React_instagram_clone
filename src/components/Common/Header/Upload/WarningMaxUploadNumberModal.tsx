6a194fd0d9ececd68097d2e554e758a3ed3a7569
kimyoungyin <yin199859@gmail.com>
2022-11-09 15:17:37 +0900

    refactor: WarningMaxUploadNumberModal UI 개선

--- src/components/Common/Header/Upload/WarningMaxUploadNumberModal.tsx
+++ src/components/Common/Header/Upload/WarningMaxUploadNumberModal.tsx
@@ -5,11 +5,22 @@ import ModalCard from "styles/UI/ModalCard";
 
 const StyledWarningMaxUploadNumberModalInner = styled.div`
     padding: 24px;
-    height: 150px;
+    height: 200px;
     display: flex;
     position: relative;
     justify-content: center;
     align-items: center;
+    & > h1 {
+        width: 100%;
+        height: 42px;
+        line-height: 42px;
+        font-size: 16px;
+        font-weight: ${(props) => props.theme.font.bold};
+        text-align: center;
+        position: absolute;
+        top: 0;
+        border-bottom: 1px solid ${(props) => props.theme.color.bd_gray};
+    }
     & > h2 {
         font-size: 18px;
     }
@@ -17,6 +28,8 @@ const StyledWarningMaxUploadNumberModalInner = styled.div`
         position: absolute;
         bottom: 14px;
         right: 14px;
+        width: 20%;
+        min-width: 80px;
     }
 `;
 
@@ -38,9 +51,10 @@ const WarningMaxUploadNumberModal = ({
             onModalOff={onModalOff}
         >
             <StyledWarningMaxUploadNumberModalInner>
+                <h1>주의</h1>
                 <h2>
                     {warnigContent === "images"
-                        ? "사진은 최대 10개까지 업로드 가능합니다"
+                        ? "사진 업로드는 최대 10개까지만 가능합니다"
                         : "각 이미지에 태그는 최대 20개까지만 가능합니다"}
                 </h2>
                 <Button onClick={onModalOff}>확인</Button>
